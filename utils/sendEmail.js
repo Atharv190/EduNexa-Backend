85a1e8e89368a6dc8d13a319b6921e2e794b4643
Atharv <atharvmarathe5@gmail.com>
2026-03-15 19:33:51 +0530

    Updated Mail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,15 +4,13 @@ export const sendEmail = async ({ to, subject, text }) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, 
+      port: 465,          // ✅ change port
+      secure: true,       // ✅ must be true for 465
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
-
-    await transporter.verify(); 
 
     const info = await transporter.sendMail({
       from: `"EduNexa Admin" <${process.env.EMAIL_USER}>`,
