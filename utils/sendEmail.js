7849e76cce1e15896a857fa96136561472673bb5
Atharv Marathe <152206405+Atharv190@users.noreply.github.com>
2026-03-16 09:06:58 +0530

    Enhance email transporter configuration
    Added TLS options and connection timeouts to email transporter.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,15 +2,20 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, text }) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.BREVO_USER,
-        pass: process.env.BREVO_PASS,
-      },
-    });
+   const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+});
 
     const info = await transporter.sendMail({
       from: `"EduNexa Admin" <maratheatharv8@gmail.com>`,
